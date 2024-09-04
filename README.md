f8a2c4e8276c791f97eb96e83027e1f9214434bd
Юрий Кузнецов <omega131995@outlook.com>
2024-09-04 12:54:07 +0800

    Обновлен README.md

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ## Возможно это будет UI библиотека
 
-Импорт стилей:
+**Импорт стилей:**
 >import "@mistek/freedom-ui/dist/assets/style.css"
+
+**Кнопки:**
+
+`<Button bg={ButtonBackground.primary}>Button</Button>`
+
+![Кнопки](https://s.iimg.su/s/04/xNLAsgQ3oI88l3pTlIWGhuwOLp0oNiWKUbuLDvyL.png)
--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import {ButtonBackground} from "../lib/components/Button/ButtonBackground.ts";
 function App() {
   return (
     <>
-        <Button bg={ButtonBackground.primary}>Button primary</Button>
-        <Button bg={ButtonBackground.secondary}>Button secondary</Button>
-        <Button bg={ButtonBackground.success}>Button success</Button>
-        <Button bg={ButtonBackground.warning}>Button warning</Button>
-        <Button bg={ButtonBackground.danger}>Button danger</Button>
+        <div style={{display: "flex", justifyContent: "space-around"}}>
+            <Button bg={ButtonBackground.primary}>Button primary</Button>
+            <Button bg={ButtonBackground.secondary}>Button secondary</Button>
+            <Button bg={ButtonBackground.success}>Button success</Button>
+            <Button bg={ButtonBackground.warning}>Button warning</Button>
+            <Button bg={ButtonBackground.danger}>Button danger</Button>
+        </div>
     </>
   )
 }
