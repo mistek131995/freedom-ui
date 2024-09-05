34db49cdb5d3a7e6c8f562337c75c11ed61538e6
Юрий Кузнецов <omega131995@outlook.com>
2024-09-05 10:56:23 +0800

    Исправлен экспорт ButtonBackground

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,4 @@
 export { Button } from './components/Button'
-export type { ButtonBackground } from './components/Button/ButtonBackground.ts'
+export { ButtonBackground } from './components/Button/ButtonBackground.ts'
 export { Input } from './components/Input'
 export { Label } from './components/Label'
