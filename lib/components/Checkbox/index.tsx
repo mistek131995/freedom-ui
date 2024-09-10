c166b651013ccc515ef44e5e48573212a0806867
Юрий Кузнецов <omega131995@outlook.com>
2024-09-10 23:39:48 +0800

    Готовый компонент checkbox

--- lib/components/Checkbox/index.tsx
+++ lib/components/Checkbox/index.tsx
@@ -1,3 +1,8 @@
-export const Checkbox = () => {
-    return <input type="checkbox"/>
+import styles from "./styles.module.scss"
+import {HTMLAttributes} from "react";
+
+export const Checkbox = (props: HTMLAttributes<HTMLInputElement>) => {
+    const unionClassName = [styles.checkbox, props.className].filter(x => x).join(" ");
+
+    return <input {...props} className={unionClassName} type="checkbox"/>
 }
